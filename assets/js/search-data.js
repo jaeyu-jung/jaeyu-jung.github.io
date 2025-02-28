9d9282dc31bf0ab59a9211573a09a9e1c73e2756
colide12 <37061518+colide12@users.noreply.github.com>
2025-02-28 04:33:14 +0000

    Deploying to gh-pages from @ colide12/colide12.github.io@5a3109a200943fb7df384f613f6720f891a87fcf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-bio",
+    title: "Bio",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
