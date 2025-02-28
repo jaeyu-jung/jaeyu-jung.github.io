2419f4ba6a1dd59ed4959d3c7b2178b7f0f8cc9b
colide12 <37061518+colide12@users.noreply.github.com>
2025-02-28 05:39:01 +0000

    Deploying to gh-pages from @ colide12/colide12.github.io@d542ff5eb1674422688f323a4ab176e80fd32dc2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,21 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "news-a-simple-inline-announcement",
+  },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
