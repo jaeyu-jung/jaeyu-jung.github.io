3afaca97bd4f2cc639626a457b5374c1bc6fb1d3
jaeyu-jung <37061518+jaeyu-jung@users.noreply.github.com>
2025-08-27 02:59:59 +0000

    Deploying to gh-pages from @ jaeyu-jung/jaeyu-jung.github.io@09dd2dc768c4383f03cee1b56f1b3baebf503408 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Courses I taught.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
